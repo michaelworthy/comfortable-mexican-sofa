a3941102adfce117f4b5dff3f17556f80c1ea578
aviranmozes <aviran@worthy.com>
2017-06-08 11:51:31 +0300

    Upgrade v-1-12-10

--- app/controllers/comfy/cms/content_controller.rb
+++ app/controllers/comfy/cms/content_controller.rb
@@ -15,12 +15,28 @@ class Comfy::Cms::ContentController < Comfy::Cms::BaseController
   rescue_from ActiveRecord::RecordNotFound, :with => :page_not_found
 
   def show
-    if @cms_page.target_page.present?
-      redirect_to @cms_page.target_page.url(:relative)
+    @cms_page = @cms_site.pages.published.find_by_full_path!("/#{params[:cms_path]}")
+
+    if @cms_page and @cms_page.layout and @cms_page.layout.identifier == 'redirect-layout' # custom redirect
+      custom_redirect_code = @cms_page.blocks.find_by_identifier('custom_redirect_code').content.to_i
+      if @cms_page.target_page.present?
+        redirect_url = @cms_page.target_page.url
+      else
+        redirect_url = @cms_page.blocks.find_by_identifier('custom_redirect_url').content
+      end
+      if custom_redirect_code > 0
+        redirect_to redirect_url, status: custom_redirect_code
+      else
+        redirect_to redirect_url
+      end
     else
-      respond_to do |format|
-        format.html { render_page }
-        format.json { render :json => @cms_page }
+      if @cms_page.target_page.present?
+        redirect_to @cms_page.target_page.url(:relative)
+      else
+        respond_to do |format|
+          format.html { render_page }
+          format.json { render :json => @cms_page }
+        end
       end
     end
   end
@@ -59,10 +75,18 @@ protected
   end
 
   def page_not_found
-    @cms_page = @cms_site.pages.published.find_by_full_path!('/404')
-
-    respond_to do |format|
-      format.html { render_page(404) }
+    full_path = request.fullpath.split('?')[0]
+    block = Comfy::Cms::Block.joins(:page => [:site]).where('cms_sites.id = ?', @cms_site.id).where(identifier: 'alternate_urls').where('cms_blocks.content LIKE (?)', "%#{full_path}%").first
+    if block.present?
+      @cms_page = block.page
+      respond_with @cms_page do |format|
+        format.html { render_html(200) }
+      end
+    else
+      @cms_page = @cms_site.pages.published.find_by_full_path!('/404')
+      respond_with @cms_page do |format|
+        format.html { render_html(404) }
+      end
     end
   rescue ActiveRecord::RecordNotFound
     raise ActionController::RoutingError.new("Page Not Found at: \"#{params[:cms_path]}\"")
